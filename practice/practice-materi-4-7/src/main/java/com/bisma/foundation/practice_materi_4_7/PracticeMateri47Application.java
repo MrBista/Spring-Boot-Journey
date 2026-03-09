5ad5962c52a3fe20f0655edb9bf90b5bf6047f5c
MrBista <gustibisman2308@gmail.com>
2026-03-09 21:09:39 +0700

    init practice in depth rest spring boot

---
+++ practice/practice-materi-4-7/src/main/java/com/bisma/foundation/practice_materi_4_7/PracticeMateri47Application.java
@@ -0,0 +1,13 @@
+package com.bisma.foundation.practice_materi_4_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeMateri47Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeMateri47Application.class, args);
+	}
+
+}
---
+++ practice/practice-materi-4-7/src/test/java/com/bisma/foundation/practice_materi_4_7/PracticeMateri47ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisma.foundation.practice_materi_4_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeMateri47ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
