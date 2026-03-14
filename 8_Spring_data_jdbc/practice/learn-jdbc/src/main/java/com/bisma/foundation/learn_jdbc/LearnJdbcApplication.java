5336bf24e2bc8456c6a2374d67f693c7aa2284ff
MrBista <gustibisman2308@gmail.com>
2026-03-14 22:57:01 +0700

    init practice learn jdbc

---
+++ 8_Spring_data_jdbc/practice/learn-jdbc/src/main/java/com/bisma/foundation/learn_jdbc/LearnJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.bisma.foundation.learn_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnJdbcApplication.class, args);
+	}
+
+}
---
+++ 8_Spring_data_jdbc/practice/learn-jdbc/src/test/java/com/bisma/foundation/learn_jdbc/LearnJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bisma.foundation.learn_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
